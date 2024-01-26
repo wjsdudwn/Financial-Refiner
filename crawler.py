3bd787447e6f8113abe5a7f1831540d914dcb0d9
youngJuJun <juyj0704@gmail.com>
2024-01-26 19:51:31 +0900

    ,

--- crawler.py
+++ crawler.py
@@ -1,19 +1,15 @@
 import selenium
-import os
+from selenium import webdriver
 from oil import oil_crawler
 
-#print(os.getcwd())
-#print("hello world")
+
 class Crawler():
-    '''
     def __init__(self) -> None:
-        # driver setting
-        # set directory of chromedriver
-        locate = os.getcwd() + "chromedriver.exe"
-        driver = selenium.driver()
-    '''
-    def oil(self):
-        return oil_crawler()
+        self.driver = webdriver.Chrome()
+        
+
+    def oil(self) -> list:
+        return oil_crawler(self.driver)
     
 
 result = Crawler().oil()
--- oil.py
+++ oil.py
@@ -6,7 +6,8 @@ import time
 import random
 def random_time():
     return random.uniform(0.5, 1)
-def oil_crawler():
+def oil_crawler(driver):
+    # user setting
     headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     }
@@ -15,41 +16,30 @@ def oil_crawler():
     options.add_argument("--disable-blink-features=AutomationControlled") #강제로 자동화 브라우저가 아님을 말함
     driver = webdriver.Chrome() #executable_path = ''
     driver.implicitly_wait(10)
+
     driver.get("https://www.financialjuice.com/home") #사이트 접속
     driver.maximize_window()
-    signin_li = driver.find_element(By.ID, 'liSignIn') # Sign in 버튼을 포함하는 li 태그 찾기
-    signin_a = signin_li.find_element(By.TAG_NAME, "a") # li 태그 안에 있는 a 태그 선택
-    signin_a.click() # 클릭해서 로그인 창으로 이동
-    input_email = driver.find_element(By.ID, "ctl00_SignInSignUp_loginForm1_inputEmail")
+    
+    # click login button
+    li = driver.find_element(By.ID, 'liSignIn') # Sign in 버튼을 포함하는 li 태그 찾기
+    a = li.find_element(By.TAG_NAME, "a") # li 태그 안에 있는 a 태그 선택
+    a.click() # 클릭해서 로그인 창으로 이동
+    # input userID & PWD
+    inputEmail = driver.find_element(By.ID, "ctl00_SignInSignUp_loginForm1_inputEmail")
     time.sleep(random_time())
-    input_password = driver.find_element(By.ID, "ctl00_SignInSignUp_loginForm1_inputPassword")
+    inputPWD = driver.find_element(By.ID, "ctl00_SignInSignUp_loginForm1_inputPassword")
     time.sleep(random_time())
-    input_email.send_keys("jhnamugeona@gmail.com")
-    input_password.send_keys("sangmoon123")
+    inputEmail.send_keys("jhnamugeona@gmail.com")
+    inputPWD.send_keys("sangmoon123")
     time.sleep(random_time())
-    login_button = driver.find_element(By.ID, "ctl00_SignInSignUp_loginForm1_btnLogin") #버튼인데 input 태그로 되어 있음
-    login_button.click()
+    loginButton = driver.find_element(By.ID, "ctl00_SignInSignUp_loginForm1_btnLogin") #버튼인데 input 태그로 되어 있음
+    loginButton.click()
     time.sleep(random_time())
-    headline_div = driver.find_element(By.CSS_SELECTOR, "col-xs-12 col-sm-12 col-md-12 col-lg-12")
-    headline_p = headline_div.find_element(By.TAG_NAME, "p")
-    headline_a = headline_p.find_element(By.TAG_NAME, 'a')
-    headline_text = headline_a.text
-    return headline_text
-
-
-    headline_elements_p = driver.find_elements(By.CLASS_NAME, 'p.headline-title') #여기서 결괏값이 안나옴!!!
-    headline_elements_a = []
-    for element in headline_elements_p:
-        headline_element_a = element.find_element(By.TAG_NAME, "a")
-        headline_elements_a.append(headline_element_a)
-    #driver.implicitly_wait(10)
-    headline_texts = [element.text for element in headline_elements_a]
-    return headline_elements_p
-    return headline_texts
-    title = driver.title
-
-
-
-    return title
-
-oil_crawler()
+    
+    # crawling
+    # headline_dziv = driver.find_element(By.CSS_SELECTOR, "col-xs-12 col-sm-12 col-md-12 col-lg-12") # none 
+    headlineTitle = driver.find_elements(By.CLASS_NAME, "headline-text")
+    result = []
+    for e in headlineTitle:
+        result.append(e.text)
+    return result
