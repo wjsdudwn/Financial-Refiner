b7a0d8d272b90a311bb4d67a869a98c3be213184
Juhyun Oh <ohjuhyun1@gmail.com>
2024-01-26 18:08:24 +0900

    18:08:

--- oil.py
+++ oil.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 import random
 def random_time():
-    return random.uniform(0.5, 1.5)
+    return random.uniform(0.5, 1)
 def oil_crawler():
     headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
@@ -24,14 +24,20 @@ def oil_crawler():
     input_password = driver.find_element(By.ID, "ctl00_SignInSignUp_loginForm1_inputPassword")
     time.sleep(random_time())
     input_email.send_keys("jhnamugeona@gmail.com")
-    time.sleep(random_time())
     input_password.send_keys("sangmoon123")
     time.sleep(random_time())
     login_button = driver.find_element(By.ID, "ctl00_SignInSignUp_loginForm1_btnLogin") #버튼인데 input 태그로 되어 있음
-    time.sleep(random_time())
     login_button.click()
-    time.sleep(60)
-    headline_list = driver.find_elements(By.CLASS_NAME, 'headline-title')
+    time.sleep(random_time())
+    headline_elements_p = driver.find_elements(By.CLASS_NAME, 'p.headline-title')
+    headline_elements_a = []
+    for element in headline_elements_p:
+        headline_element_a = element.find_element(By.TAG_NAME, "a")
+        headline_elements_a.append(headline_element_a)
+    #driver.implicitly_wait(10)
+    headline_texts = [element.text for element in headline_elements_a]
+    return headline_elements_p
+    return headline_texts
     title = driver.title
 
 
