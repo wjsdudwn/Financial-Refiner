e2e5067ab369433021901a9029b8e2aed783cdd1
Juhyun Oh <ohjuhyun1@gmail.com>
2024-02-01 20:12:57 +0900

    Telegram message send trial 1

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from crawler import Crawler
 import time
+import telegram
 
 prev = []  # list current news
 cur = [] # list before 10sec news
+
 while True:
     cur = Crawler().oil() # save current news
     
@@ -13,9 +15,15 @@ while True:
     prev = cur.copy() # else > update prev list
 
     # =========================== telegram code
+    bot = telegram.Bot(token="6775298069:AAHjq49Sui3-mitDJ-siLIKmP6TD1gle1oo")
+    bot.send_message(chat_id = "1002139751230", text="Hello world")
 
 
-
+    print("Sent a message")
     # ===========================
 
     time.sleep(10)
+
+bot = telegram.Bot(token="6775298069:AAHjq49Sui3-mitDJ-siLIKmP6TD1gle1oo")
+bot.send_message(chat_id = 1002139751230, text="Hello world")
+print("Sent a message")
